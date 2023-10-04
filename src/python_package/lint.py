509322686ac18297bbaa5febbeb17ef30e2ed818
snickk95 <harrisonnick16@gmail.com>
2023-10-04 16:38:00 -0400

    lint.py script

---
+++ src/python_package/lint.py
@@ -0,0 +1,22 @@
+#lint.py
+
+from __future__ import annotations
+
+import sys
+
+from pylint import lint
+
+THRESHOLD = 9
+
+run = lint.Run(["hello_world.py"], do_exit=False)
+
+score = run.linter.stats.global_note
+
+if score < THRESHOLD:
+
+    print("Linter failed: Score < threshold value")
+
+    sys.exit(1)
+
+
+sys.exit(0)
